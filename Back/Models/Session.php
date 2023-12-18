aa9bea6e0830a16f98d10712afb0745441e2116e
AlexisRoyGit <alexisroypro@outlook.fr>
2023-12-18 13:41:39 +0100

    Code de la classe Session

---
+++ Back/Models/Session.php
@@ -0,0 +1,30 @@
+<?php
+
+class Session 
+{
+
+    public function linksNav() 
+    {
+        if(isset($_SESSION['userIdentity'])) {
+            echo '<li><a href="espace.html">Votre espace</a></li>';
+        } else {
+            echo '<li><a href="connexion.html">Connexion</a></li>';
+        }
+    }
+
+    public function linksMobile() 
+    {
+        if(isset($_SESSION['userIdentity'])) {
+            echo '<a href="espace.html" class="undisplayed desktopconnexion">Votre espace</a>';
+        } else {
+            echo '<a href="connexion.html" class="undisplayed desktopconnexion">Connexion</a>';
+        }
+    }
+
+    public function deconnexion() 
+    {
+        unset($_SESSION['userIdentity']);
+        unset($_COOKIE['PHPSESSID']);
+        setcookie('PHPSESSID', '', time() - 3600);
+    }
+}
