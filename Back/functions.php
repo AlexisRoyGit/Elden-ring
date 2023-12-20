57d50ecbae8da0a49ed4dfca33c2645b17c9379b
AlexisRoyGit <alexisroypro@outlook.fr>
2023-12-20 11:17:28 +0100

    Fonctions verifications avatar

--- Back/functions.php
+++ Back/functions.php
@@ -1,17 +1,40 @@
 <?php
 
-function lengthPassword(string $pass) {
+function lengthPassword(string $pass): bool {
     return strlen($pass) > 10 && strlen($pass) < 20;
 }
 
-function charactersPasword(string $pass) {
+function charactersPasword(string $pass): bool {
     $maj = '/[A-Z]/';
     $number = '/[0-9]/';
     
     return boolval(preg_match($maj, $pass) && preg_match($number, $pass));
 }
 
-function emailValid(string $mail) {
+function emailValid(string $mail): bool {
     $pattern = '/^[A-z0-9_.+-]+@[A-z0-9-]+\.[A-z0-9-.]+$/';
     return boolval(preg_match($pattern, $mail));
 }
+
+function fileWeight(array $file): bool {
+    return $file['size'] < 4194304; 
+}
+
+function fileExtesion(array $file): bool {
+    $pattern = '/\.(jpg|jpeg|png|gif)$/';
+    return boolval(preg_match($pattern, $file['name']));
+}
+
+function fileType(array $file): bool {
+    return $file['type'] == "image/gif" || $file['type'] == "image/jpeg" || $file['type'] == "image/jpg" || $file['type'] == "image/png";
+}
+
+function fileError(array $file): bool {
+    return $file['error'] == 0;
+}
+
+function fileVerifications(array $file): bool {
+    return fileWeight($file) && fileExtesion($file) && fileType($file) && fileError($file);
+}
+
+//FONCTIONS CHAMPS VIDES
